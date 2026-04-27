eb326d34cc96f8c9b902470aa976197a0c9642d1
Kiran <133772510+kian-collab@users.noreply.github.com>
2026-04-27 11:30:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -53,6 +53,6 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0' ,() => {
     console.log(`Movie tickets app running on http://localhost:${PORT}`);
 });
